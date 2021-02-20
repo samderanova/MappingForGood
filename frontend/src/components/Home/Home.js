59ce814a26884b8977b6f28f167c2559c448a023
ShanMehr <ishanmeher25@gmail.com>
2021-02-20 12:04:10 -0800

    Dark Theme Made

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -20,23 +20,18 @@ export default class Home extends React.Component {
                 </header>
                 <main>
                     <div className="box-1" id="#problem">
-                        <h2 >The Problem</h2>
+                        
                         <div className="box-1-left card card-font">
+                        <h2 >The Problem</h2>
                         <p>
                                     Poverty is a persisting problem throughout the United States with over 46.2 million Americans living in poverty according to the US Census
                                 and with the emergence of Covid-19 that number has only grown. In hope of reducing the economic inequity within 
                                 Sacramento our group has created a map that displays the various regions that face poverty
-                            </p>
-                            <p>
-                                Our team used several metrics to decide whether a region in Sacramento needed aid. 
-                            One of the metrics we used was income since areas of low income often lack the resources necessary to provide the people with a better lifestyle.
-                            We also use the metric of determining whether an area is a food desert. An area is classified as a food desert if there are no supermarkets present within a 1-mile radius of the area. 
-                            These areas are often overlooked by supermarket companies as the consumer base is unable to afford food and thus the people are unable to access healthy meals.
-                            </p>
-                            <p>
+                        </p>
+                        <p>
                             The Community College Student from Sacramento noticed the economic inequality while living in Sacramento such as the large amount of homeless camps and the differences in living conditions that span across various neighborhoods within Sacramento and was motivated to create a web app that aimed to solve some of these problem by pinpointing areas within Sacramento that needed aid and directing government agencies and nonprofit organizations to provide aid to these areas. The other members of our team were overjoyed to take on a project that would focus on a fulfilling goal of improving the living conditions of others.
 
-                            </p>
+                        </p>
                         </div>
                         <div className="box-1-right">
                             <img src="https://kubrick.htvapps.com/vidthumb/0d8ba403-2af2-4608-9f85-967929330f74/0d8ba403-2af2-4608-9f85-967929330f74_image.jpg?crop=0.775xw:0.774xh;0.0718xw,0&resize=1200:*" width="500" height="500" style={{backgroundColor: 'gray'}} alt="Map of "></img>
@@ -48,13 +43,14 @@ export default class Home extends React.Component {
                             <p>To help solve this problem, we created a map that maps poverty across the city of 
                                 Sacramento, CA. Some metrics we used were:
                                 <ul>
-                                    <li>Food deserts in areas with limited access to food</li>
-                                    <li>Income levels</li>
+                                    <li>Food deserts are areas with limited access to nutritious and affordable food.
+                                        In particular an area is classified as a food desert if a neighborhood is more than 1 mile away from a supermarket.
+                                    </li>
+                                    <li>We also used income data from the US census to determine areas of economic inequity</li>
                                 </ul>
-                                With these metrics, our map can provide informative advice to government
-                                agencies and nonprofit organizations to help them pinpoint which areas need
-                                aid the most.
+                                By using these metrics our team aims to be able to pinpoint areas that are in need of additonal aid in the form of social programs or donations.
                             </p>
+                            <p>We also acknowledge that many areas can be overlooked by Census and other federal datasets, and so our team created a message board in which people can communicate the needs of their communities.</p>
                         </div>
                         <div className="box-2-right">
                             <img src=""></img>
