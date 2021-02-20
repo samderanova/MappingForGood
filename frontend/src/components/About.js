c3a9497d3e090e04eec92f05b90029a0d51e0c99
ShanMehr <ishanmeher25@gmail.com>
2021-02-19 22:55:22 -0800

    About Page

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -2,6 +2,12 @@ import React from 'react';
 
 export default function About() {
     return(
-        <div>About</div>
+    <div className="About">
+      <h1>Our Mission</h1>
+    <p>
+        Insert Mission Statement Here
+    </p>
+        
+    </div>
     )
 }
